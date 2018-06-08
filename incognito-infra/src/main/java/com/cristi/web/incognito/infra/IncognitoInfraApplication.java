b9bb6072c2b0a464cfdb9851b0e4c359458509a9
cristi-zmf <cristi_zmf@yahoo.com>
2018-06-09 02:05:22 +0300

    Managed to add infra submodule but dependencies are not working as should

---
+++ incognito-infra/src/main/java/com/cristi/web/incognito/infra/IncognitoInfraApplication.java
@@ -0,0 +1,12 @@
+package com.cristi.web.incognito.infra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncognitoInfraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncognitoInfraApplication.class, args);
+	}
+}
