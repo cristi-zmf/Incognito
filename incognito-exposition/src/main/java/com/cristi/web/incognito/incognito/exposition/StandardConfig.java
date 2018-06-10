19e86f99bbc74f66d5bbd62731ab0d2e2ff9c516
cristi-zmf <cristi_zmf@yahoo.com>
2018-06-11 00:48:47 +0300

    configure expo as a war

---
+++ incognito-exposition/src/main/java/com/cristi/web/incognito/incognito/exposition/StandardConfig.java
@@ -0,0 +1,9 @@
+package com.cristi.web.incognito.incognito.exposition;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.cristi.web.incognito")
+public class StandardConfig {
+}
