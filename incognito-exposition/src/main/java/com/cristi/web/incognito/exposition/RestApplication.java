7a099726fabeaf87ceab5d1d111d6a32b48d85fb
cristi-zmf <cristi_zmf@yahoo.com>
2018-09-10 23:46:30 +0300

    resource is working finally

--- incognito-exposition/src/main/java/com/cristi/web/incognito/exposition/RestApplication.java
+++ incognito-exposition/src/main/java/com/cristi/web/incognito/exposition/RestApplication.java
@@ -1,22 +1,20 @@
 package com.cristi.web.incognito.exposition;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@StandardConfig
-@EnableAutoConfiguration
+//@StandardConfig
+@SpringBootApplication
 @ComponentScan(basePackages = "com.cristi.web.incognito")
 public class RestApplication extends SpringBootServletInitializer {
-    private static Class<RestApplication> applicationClass = RestApplication.class;
+//    private static Class<RestApplication> applicationClass = RestApplication.class;
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
     }
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(applicationClass);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(applicationClass);
+//    }
 }
---
+++ incognito-exposition/src/main/java/com/cristi/web/incognito/exposition/resource/FirstResource.java
@@ -0,0 +1,22 @@
+package com.cristi.web.incognito.exposition.resource;
+
+import com.cristi.web.incognito.exposition.Dto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.lang.String.format;
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@RequestMapping("/api")
+public class FirstResource {
+    @PostMapping(path = "/test")
+//    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    public ResponseEntity<String> getDto(@RequestBody Dto dto) {
+        System.out.printf(format("Got the dto: value: %s , object: %s", dto.value, dto.object));
+        return ok(dto.value);
+    }
+}
