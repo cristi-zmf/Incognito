4ef4e7d55b9bf7d4523add0b828af981b96177de
cristi-zmf <cristi_zmf@yahoo.com>
2018-10-14 23:58:19 +0300

    removed useless imports

--- incognito-infra/src/main/java/com/cristi/web/incognito/infra/persistence/changelogs/common/SubjectChangelog.java
+++ incognito-infra/src/main/java/com/cristi/web/incognito/infra/persistence/changelogs/common/SubjectChangelog.java
@@ -2,9 +2,7 @@ package com.cristi.web.incognito.infra.persistence.changelogs.common;
 
 import com.cristi.web.incognito.domain.subject.Description;
 import com.cristi.web.incognito.domain.subject.Subject;
-import com.cristi.web.incognito.domain.subject.Subjects;
 import com.cristi.web.incognito.domain.subject.Title;
-import com.cristi.web.incognito.infra.persistence.subject.MongoSubjects;
 import com.github.mongobee.changeset.ChangeLog;
 import com.github.mongobee.changeset.ChangeSet;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -13,9 +11,6 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 public class SubjectChangelog {
     @ChangeSet(order = "001", id = "someChangeWithJongo", author = "testAuthor")
     public void initialDataSetForSubjectCollection( MongoTemplate mongoTemplate) {
-        // type: org.jongo.Jongo : Jongo driver can be used, used for simpler notation
-        // example:
-
         Subject subject = new Subject(new Title("iPhone xx"), new Description("so xx!!!!!"));
         mongoTemplate.save(subject);
     }
